aff29c9bd19fc0daaa27df2d76980d203b654380
Ry Sutton <ryancesutton@gmail.com>
2021-08-03 10:09:23 -0500

    uncomment and fix variable scoping issues

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,89 +4,90 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-//    int i = 5;
-//
-//    while (i <= 15) {
-//        System.out.print(i + " ");
-//        i++;
-//    }
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
 
 
-//    int counter = 0;
-//
-//    do {
-//        System.out.println(counter);
-//        counter += 2;
-//        counter = counter + 2;
-//    }while (counter <= 100);
+        int counter = 0;
 
-//      int backwardsCounter = 100;
-//
-//      do {
-//          System.out.println(backwardsCounter);
-//          backwardsCounter -= 5;
-//
-//      }while (backwardsCounter >= -10);
-
-//        for (int i = 100; i >= -10; i -= 5) {
-//            System.out.println(i);
-//        }
-
-//        long twoCounter = 2L;
-//
-//        do {
-//            System.out.println(twoCounter);
-//            twoCounter *= twoCounter;
-//            twoCounter = twoCounter * twoCounter;
-//        }while (twoCounter < 1000000L);
-
-//        for (long i = 2L; i < 1000000L; i *= i){
-//            System.out.println(i);
-//        }
+        do {
+            System.out.println(counter);
+            counter += 2;
+            counter = counter + 2;
+        } while (counter <= 100);
 
 
-//        for (int i = 1; i <= 100; i++){
-//            if (i % 5 == 0 && i % 3 == 0){
-//                System.out.println("FizzBuzz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else {
-//                System.out.println(i);
-//            }
-//
-//        }
+        int backwardsCounter = 100;
+
+        do {
+            System.out.println(backwardsCounter);
+            backwardsCounter -= 5;
+
+        } while (backwardsCounter >= -10);
+
+        for (int backCount = 100; backCount >= -10; backCount -= 5) {
+            System.out.println(backCount);
+        }
+
+        long twoCounter = 2L;
+
+        do {
+            System.out.println(twoCounter);
+            twoCounter *= twoCounter;
+            twoCounter = twoCounter * twoCounter;
+        } while (twoCounter < 1000000L);
+
+        for (long longI = 2L; longI < 1000000L; longI *= longI) {
+            System.out.println(longI);
+        }
+
+
+        for (int fizzI = 1; fizzI <= 100; fizzI++) {
+            if (fizzI % 5 == 0 && fizzI % 3 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (fizzI % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (fizzI % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(fizzI);
+            }
+
+        }
 
 
         Scanner scanner = new Scanner(System.in);
-//
-//        boolean userContinues = true;
-//
-//        do {
-//            System.out.println("What number would you like to go up to?");
-//            int userInt = scanner.nextInt();
-//            System.out.println("\nHere is your table!\n");
-//            System.out.println("number | squared | cubed");
-//            System.out.println("------ | ------- | -----");
-//            for (int i = 1; i <= userInt; i++) {
-//                System.out.printf("%-6d", i);
-//                System.out.print(" | ");
-//                System.out.printf("%-7d", i * i);
-//                System.out.print(" | ");
-//                System.out.print(i * i * i);
-//                //Docrob alternate table display
-////                System.out.printf("%-6d | %-7d | %-5d\n", i, i*i, i*i*i);
-//                System.out.println();
-//            }
-//            System.out.println("Would you like to enter another number (y/n)?");
-//            String userResponse = scanner.next();
-//            if (!userResponse.equalsIgnoreCase("y")){
-//                userContinues = false;
-//            }
-//
-//
-//        }while(userContinues);
+
+        boolean userContinues = true;
+
+        do {
+            System.out.println("What number would you like to go up to?");
+            int userInt = scanner.nextInt();
+            System.out.println("\nHere is your table!\n");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int numI = 1; numI <= userInt; numI++) {
+                System.out.printf("%-6d", numI);
+                System.out.print(" | ");
+                System.out.printf("%-7d", numI * numI);
+                System.out.print(" | ");
+                System.out.print(numI * numI * numI);
+                //Docrob alternate table display
+                //System.out.printf("%-6d | %-7d | %-5d\n", i, i*i, i*i*i);
+                System.out.println();
+            }
+            System.out.println("Would you like to enter another number (y/n)?");
+            String userResponse = scanner.next();
+            if (!userResponse.equalsIgnoreCase("y")) {
+                userContinues = false;
+            }
+
+
+        } while (userContinues);
 
 
         boolean anotherGrade = true;
@@ -113,22 +114,12 @@ public class ControlFlowExercises {
             String userResponse = scanner.next();
 
 
-            if (!userResponse.equalsIgnoreCase("y")){
+            if (!userResponse.equalsIgnoreCase("y")) {
                 anotherGrade = false;
             }
 
 
-        }while (anotherGrade);
-
-
-
-
-
-
-
-
-
-
+        } while (anotherGrade);
 
 
     }
