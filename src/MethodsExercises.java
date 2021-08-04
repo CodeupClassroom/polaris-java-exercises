4a766e7e9d2429490597178d4068059c1f4383d9
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-08-04 10:45:33 -0500

    finished exercise 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static double Addition(double numOne, double numTwo){
@@ -33,6 +35,47 @@ public class MethodsExercises {
         return numOne % numTwo;
     }
 
+    public static int getIntegerRecursion(int min, int max){
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter a number between "+ min +" and "+ max +": ");
+        int userResponse = userInput.nextInt();
+
+        if (userResponse >= min && userResponse <= max){
+            return userResponse;
+        }else {
+            System.out.println(userResponse +" is not within the range.");
+            return getInteger(min,max);
+        }
+
+    }
+
+    public static int getInteger(int min, int max){
+
+        Scanner userInput = new Scanner(System.in);
+
+        int userResponse = min - 1;
+        boolean hasLoopRanOnce = false;
+
+        do {
+
+            if (hasLoopRanOnce){
+                System.out.println(userResponse +" is not within the range.");
+            }
+
+            System.out.print("Enter a number between "+ min +" and "+ max +": ");
+            userResponse = userInput.nextInt();
+
+            hasLoopRanOnce = true;
+
+        }while (!(userResponse >= min && userResponse <= max));
+
+        return userResponse;
+
+    }
+
+
     public static void main(String[] args) {
 
         System.out.println(Addition(59.38,2.833));
@@ -41,6 +84,8 @@ public class MethodsExercises {
         System.out.println(Division(120,3));
         System.out.println(Modulus(120,0));
 
+
+        System.out.println("Your number in the range was " + getInteger(1,100));
 
     }
 
