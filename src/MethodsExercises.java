8249f028ed2a5ec687f44f1d55a24e75f7c2de45
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-08-04 11:27:47 -0500

    finished exercise 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -75,17 +75,69 @@ public class MethodsExercises {
 
     }
 
+    public static String getFactorialString(int num){
+
+        String stringToReturn = "";
+
+        for (int i = 1; i <= num; i++){
+
+            if (i == num){
+                stringToReturn += i;
+            }else {
+                stringToReturn += i + " x ";
+            }
+
+        }
+
+        return stringToReturn;
+    }
+
+    public static int getFactorialSum(int num){
+
+        int sum = 1;
+
+        for (int i = 2; i <= num; i++){
+
+          sum *= i;
+
+        }
+
+        return sum;
+
+    }
+
+    public static void getFactorial(){
+
+        int factorialNumber = getInteger(1,10);
+
+        for (int i = 1; i <= factorialNumber; i++){
+
+//            System.out.printf("%d! = %s = %d\n",i,getFactorialString(i),getFactorialSum(i));
+            System.out.printf("%d! =",i);
+            System.out.printf(" %-20s",getFactorialString(i));
+            System.out.printf("= %d\n",getFactorialSum(i));
+
+        }
+
+    }
+
 
     public static void main(String[] args) {
 
-        System.out.println(Addition(59.38,2.833));
-        System.out.println(Subtraction(10,5.5));
-        System.out.println(Multiplication(7.25,8));
-        System.out.println(Division(120,3));
-        System.out.println(Modulus(120,0));
+//        System.out.println(Addition(59.38,2.833));
+//        System.out.println(Subtraction(10,5.5));
+//        System.out.println(Multiplication(7.25,8));
+//        System.out.println(Division(120,3));
+//        System.out.println(Modulus(120,0));
+//
+//
+//        System.out.println("Your number in the range was " + getInteger(1,100));
 
+        getFactorial();
 
-        System.out.println("Your number in the range was " + getInteger(1,100));
+//        System.out.println(getFactorialString(4));
+
+//        System.out.println(getFactorialSum(3));
 
     }
 
