b1123ab985caa2ca69774eb8164c4b195261a9e6
docrob <mark.robinson@utsa.edu>
2021-08-09 09:43:38 -0500

    abstraction lecture plan

---
+++ src/lecture_stuff/AbstractionCompleted.java
@@ -0,0 +1,48 @@
+package lecture_stuff;
+
+public class AbstractionCompleted {
+    public static void main(String[] args) {
+        /*
+        *** introduce abstract
+
+        abstract keyword on classes
+            explain and show (on Animal)
+
+        abstract keyword on methods
+            explain and show (on Animal)
+            makeNoise() is abstract
+            walk() is non-abstract
+            breathe is a final non-abstract method
+
+        make an animal farm class
+
+        try to instantiate an Animal
+
+        make a pig subclass
+
+        make a chicken subclass
+
+        show array of abstract data types
+            add a pig and a chicken
+            iterate over the array
+            have each make a noise
+
+        *** introduce the interface
+            NO instance variables (only constants)
+            all methods by default are PUBLIC and ABSTRACT
+
+        define interface for FlyingAnimal
+            explain history of "...able" names
+
+        add FlyingAnimal to chicken
+
+        add a Duck class
+
+        show array of FlyingAnimals
+
+
+
+
+         */
+    }
+}
