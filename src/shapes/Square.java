4099c7b8ac526570497cf0979282f247cd609da6
Ry Sutton <ryancesutton@gmail.com>
2021-08-10 09:23:27 -0500

    completed abstract excercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,24 +1,30 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 
-    public Square (int side) {
+    public Square (double side) {
         super(side, side);
     }
 
 
     @Override
-    public int getArea(){
-        System.out.println("Get area from square");
-        return super.length * super.width;
+    public double getPerimeter() {
+        return 4 * this.length;
     }
 
     @Override
-    public int getPerimeter(){
-        System.out.println("Get per from square");
-        return 4 * super.length;
+    public double getArea() {
+        return this.length * this.width;
     }
 
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
 
-
+    @Override
+    public void setWidth(double width) {
+        this.setLength(width);
+    }
 }
